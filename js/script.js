7b1bfdca2e2eff45cfef874ece487028adb89042
Luca <lucafagiolini@live.it>
2024-02-07 19:58:47 +0100

    toggle on cell added

--- js/script.js
+++ js/script.js
@@ -13,12 +13,28 @@ for (let i = 0; i < 100; i++) {
     // crate 100 celle e disposte in pagina 
     const cellElement = document.createElement('div');
     cellElement.classList.add('cell');
-    gridElement.appendChild(cellElement);
+    cellElement.classList.add('btn');
+    cellElement.classList.add('btn-info');
+
+    gridElement.append(cellElement);
+
+    // inserisco il nummero della cella 
+    cellElement.textContent = i + 1;
 
     // inserisco le celle nell'array
     gridArray.push(cellElement);
-    console.log(gridArray);
+
+    // creo un evento click per ogni cella
+    cellElement.addEventListener('click', function () {
+        console.log('click');
+
+        this.classList.toggle('active');
+        console.log(this);
+    });
+
+
 }
+console.log(gridArray);
 
 
 

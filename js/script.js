5c7be659c0ede1ccbf3ff531c725e2fe5a763fd6
Luca <lucafagiolini@live.it>
2024-02-07 20:56:55 +0100

    exercise completed

--- js/script.js
+++ js/script.js
@@ -5,36 +5,39 @@ const gridElement = document.querySelector('#grid');
 let gridArray = [];
 
 
+// collego un bottone per generare la griglia al click
+document.querySelector("#start-btn").addEventListener('click', function () {
+    console.log('game started!');
 
-// creo gli elementi che andranno a comporre la griglia con un ciclo for 
-// la griglia sarà composta da 100 celle, ogni riga dovrà contenere 10 celle 
+    // creo gli elementi che andranno a comporre la griglia con un ciclo for 
+    // la griglia sarà composta da 100 celle, ogni riga dovrà contenere 10 celle 
 
-for (let i = 0; i < 100; i++) {
-    // crate 100 celle e disposte in pagina 
-    const cellElement = document.createElement('div');
-    cellElement.classList.add('cell');
-    cellElement.classList.add('btn');
-    cellElement.classList.add('btn-info');
+    for (let i = 0; i < 100; i++) {
+        // crate 100 celle e disposte in pagina 
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        cellElement.classList.add('btn');
+        cellElement.classList.add('btn-info');
 
-    gridElement.append(cellElement);
+        gridElement.append(cellElement);
 
-    // inserisco il nummero della cella 
-    cellElement.textContent = i + 1;
-
-    // inserisco le celle nell'array
-    gridArray.push(cellElement);
-
-    // creo un evento click per ogni cella
-    cellElement.addEventListener('click', function () {
-        console.log('click');
-
-        this.classList.toggle('active');
-        console.log(this);
-    });
+        // inserisco il nummero della cella 
+        cellElement.textContent = i + 1;
 
 
-}
-console.log(gridArray);
+        // inserisco le celle nell'array
+        gridArray.push(cellElement);
 
+        // creo un evento click per ogni cella
+        cellElement.addEventListener('click', function () {
+            console.log('this is the cell number: +' + (i + 1));
+
+            this.classList.toggle('active');
+
+        });
+
+    }
+
+});
 
 

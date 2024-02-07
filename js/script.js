2d641e0886ea01717c3257591d1e306fb6704d7c
Luca <lucafagiolini@live.it>
2024-02-07 18:38:08 +0100

    array created

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 // seleziono la griglia
 const gridElement = document.querySelector('#grid');
-console.log(gridElement);
+
+// creo un array per numerare le celle
+let gridArray = [];
+
 
 
 // creo gli elementi che andranno a comporre la griglia con un ciclo for 
 // la griglia sarà composta da 100 celle, ogni riga dovrà contenere 10 celle 
 
 for (let i = 0; i < 100; i++) {
+    // crate 100 celle e disposte in pagina 
     const cellElement = document.createElement('div');
-    console.log(cellElement);
     cellElement.classList.add('cell');
     gridElement.appendChild(cellElement);
+
+    // inserisco le celle nell'array
+    gridArray.push(cellElement);
+    console.log(gridArray);
 }
+
+
 

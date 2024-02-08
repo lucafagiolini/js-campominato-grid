64a079d00ea9251ba83e50a20238584af434bb04
Luca <lucafagiolini@live.it>
2024-02-08 01:54:43 +0100

    bonus checkpoint

--- js/script.js
+++ js/script.js
@@ -4,15 +4,70 @@ const gridElement = document.querySelector('#grid');
 // creo un array per numerare le celle
 let gridArray = [];
 
+// dichiaro una variabile per la grandezza della griglia dal bottone "#diffculty selection"
+const grideSizeSelection = document.querySelector('#difficulty-selection');
+
+
+document.querySelector('#easy-diff').addEventListener('click', function () {
+    this.classList.toggle('active');
+    console.log('easy selected');
+
+    if (this.classList.contains('active')) {
+        document.querySelector('#medium-diff').classList.remove('active');
+        document.querySelector('#hard-diff').classList.remove('active');
+    }
+
+});
+
+document.querySelector('#medium-diff').addEventListener('click', function () {
+    this.classList.toggle('active');
+    console.log('medium selected');
+
+    if (this.classList.contains('active')) {
+        document.querySelector('#easy-diff').classList.remove('active');
+        document.querySelector('#hard-diff').classList.remove('active');
+    }
+});
+
+document.querySelector('#hard-diff').addEventListener('click', function () {
+    this.classList.toggle('active');
+    console.log('hard selected');
+
+    if (this.classList.contains('active')) {
+        document.querySelector('#easy-diff').classList.remove('active');
+        document.querySelector('#medium-diff').classList.remove('active');
+    }
+});
+
+
+
+function gridSize() {
+    if (document.querySelector('#easy-diff').classList.contains('active')) {
+        return 100
+
+    } else if (document.querySelector('#medium-diff').classList.contains('active')) {
+        return 81;
+
+    } else if (document.querySelector('#hard-diff').classList.contains('active')) {
+        return 49;
+    }
+
+}
+console.log(gridSize());
+
+
+
+
 
 // collego un bottone per generare la griglia al click
 document.querySelector("#start-btn").addEventListener('click', function () {
     console.log('game started!');
 
+
+
     // creo gli elementi che andranno a comporre la griglia con un ciclo for 
     // la griglia sarà composta da 100 celle, ogni riga dovrà contenere 10 celle 
-
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < gridSize; i++) {
         // crate 100 celle e disposte in pagina 
         const cellElement = document.createElement('div');
         cellElement.classList.add('cell');
